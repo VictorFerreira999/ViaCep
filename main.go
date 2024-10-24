b3599629b5acd55dd8843bcbc1a1d773a97e3480
VictorFerreira999 <victorferreira9990@gmail.com>
2024-10-24 09:36:54 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+
+func cepHandler(w http.ResponseWriter, r *http.Request) {
+	cep := r.URL.Query().Get("cep") 
+
+	resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+	if err != nil {
+		http.Error(w, "Erro ao consultar o ViaCEP", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "CEP inválido ou não encontrado", http.StatusBadRequest)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Erro ao ler a resposta do ViaCEP", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
+}
+
+func main() {
+	http.HandleFunc("/cep", cepHandler)
+
+	fmt.Println("Servidor rodando na porta 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil)) 
+}
